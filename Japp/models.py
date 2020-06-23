76ba0ddb760fb0979f93efe79af95ad3f5d041f4
Zqijing <your_email_here>
2020-06-22 20:45:56 -0700

    Changed project file name

--- Japp/models.py
+++ Japp/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=30)
+    email = models.EmailField()
+    time_create = models.DateTimeField(auto_now_add=True)
+
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Outdoor', 'Outdoor')
+    )
+
+    name = models.CharField(max_length=30)
+    price = models.CharField(max_length=30)
+    category = models.CharField(max_length=30, choices=CATEGORY)
+    description = models.TextField(blank=True, null=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivered', 'Delivered')
+    )
+
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='c_order')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='p_order')
+    time_created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=30, choices=STATUS)
