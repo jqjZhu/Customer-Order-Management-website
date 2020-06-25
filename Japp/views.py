7a652d5085b0bc41e1cec4b5868911c0b9882c4f
Zqijing <your_email_here>
2020-06-24 18:55:23 -0700

    Make dynamic UI

--- Japp/views.py
+++ Japp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Customer, Product, Order
 from .forms import CustomerForm, OrderForm
 
@@ -41,8 +41,34 @@ def create(request):
     c_form = CustomerForm()
     o_form = OrderForm()
 
+    if request.method == 'POST':
+        if 'c-button' in request.POST:
+            c_form = CustomerForm(request.POST)
+            if c_form.is_valid():
+                name_get = c_form.cleaned_data['name']
+                phone_get = c_form.cleaned_data['phone']
+                email_get = c_form.cleaned_data['email']
+                customer = Customer.objects.create(name=name_get,
+                                                   phone=phone_get,
+                                                   email=email_get)
+                customer.save()
+                return redirect('homepage')
+
+        if 'o-button' in request.POST:
+            o_form = OrderForm(request.POST)
+            if o_form.is_valid():
+                customer_get = o_form.cleaned_data['customer']
+                product_get = o_form.cleaned_data['product']
+                status_get = o_form.cleaned_data['status']
+                order = Order.objects.create(customer=customer_get,
+                                             product=product_get,
+                                             status=status_get)
+                order.save()
+                return redirect('homepage')
+
     context = {
         'c_form': c_form,
         'o_form': o_form
     }
+
     return render(request, 'Japp/create.html', context)
